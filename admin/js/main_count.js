f4e739a45b93bea392f240c0696bbc6d9a10959d
雪花 <2291936832@qq.com>
2020-03-09 00:58:05 +0800

    主题页的渲染

--- admin/js/main_count.js
+++ admin/js/main_count.js
@@ -1,5 +1,5 @@
 $(function() {
-  //发送ajax请求获得数据
+  //发送ajax请求获得数据.月新增文章
   $.ajax({
     type: "get",
     url: BigNews.data_article,
@@ -38,11 +38,11 @@ $(function() {
           {
             data: count,
             type: "line",
-            name: "新增文章" //给小圆点显示名字用的
-            // areaStyle: {
-            //里面设置颜色用的
-            //   backgroundColor: " #cccccc"
-            // }
+            name: "新增文章", //给小圆点显示名字用的
+            areaStyle: {
+              //里面设置颜色用的
+              backgroundColor: " #cccccc"
+            }
           }
         ],
         tooltip: {
@@ -52,6 +52,95 @@ $(function() {
             return [pt[0], "10%"];
           }
         }
+      };
+      // 使用刚指定的配置项和数据显示图表。这里也是不会变得
+      myChart.setOption(option);
+    }
+  });
+  //.所有文章的统计
+  $.ajax({
+    type: "get",
+    url: BigNews.data_info,
+
+    success: function(response) {
+      console.log(response);
+      //模板引擎
+      let htmlstr = template("info", response);
+
+      $(".container-fluid1").html(htmlstr);
+    }
+  });
+  //各分类文章总数
+  $.ajax({
+    type: "get",
+    url: BigNews.data_category,
+
+    success: function(response) {
+      console.log(response);
+      let date = response.date.map(function(item) {
+        return { value: item.articles, name: item.name };
+      });
+      console.log(date);
+
+      //饼图
+      let myChart = echarts.init(document.getElementById("pie_show")); //初始化,这里是不会变的
+      option = {
+        backgroundColor: "#2c343c",
+
+        title: {
+          text: "分类文章数量比",
+          left: "center", //文章标题居中
+          top: 20,
+          textStyle: {
+            color: "#ccc"
+          }
+        },
+
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+
+        visualMap: {
+          show: false,
+          min: 80,
+          max: 600,
+          inRange: {
+            colorLightness: [0, 1]
+          }
+        },
+        series: [
+          {
+            name: "分类名称",
+            type: "pie",
+            radius: "55%",
+            center: ["50%", "50%"],
+            data: date,
+            roseType: "radius",
+            label: {
+              color: "rgba(255, 255, 255, 0.3)"
+            },
+            labelLine: {
+              lineStyle: {
+                color: "rgba(255, 255, 255, 0.3)"
+              },
+              smooth: 0.2,
+              length: 10,
+              length2: 20
+            },
+            itemStyle: {
+              color: "#c23531",
+              shadowBlur: 200,
+              shadowColor: "rgba(0, 0, 0, 0.5)"
+            },
+
+            animationType: "scale",
+            animationEasing: "elasticOut",
+            animationDelay: function(idx) {
+              return Math.random() * 200;
+            }
+          }
+        ]
       };
       // 使用刚指定的配置项和数据显示图表。这里也是不会变得
       myChart.setOption(option);
