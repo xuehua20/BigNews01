7c03c71a5552d1d39340f9aed553964b5658b491
雪花 <2291936832@qq.com>
2020-03-14 17:47:19 +0800

    评论页完成

--- admin/js/main_count.js
+++ admin/js/main_count.js
@@ -5,9 +5,8 @@ $(function() {
     url: BigNews.data_article,
 
     success: function(response) {
-      console.log(response);
       let time = response.date;
-      console.log(time);
+
       //使用map方法映射一个新的数组出来
       let date = time.map(item => {
         return item.date;
@@ -15,7 +14,6 @@ $(function() {
       let count = time.map(item => {
         return item.count;
       });
-      console.log(count);
 
       //折线图
       let myChart = echarts.init(document.getElementById("main")); //初始化,这里是不会变的
@@ -63,7 +61,6 @@ $(function() {
     url: BigNews.data_info,
 
     success: function(response) {
-      console.log(response);
       //模板引擎
       let htmlstr = template("info", response);
 
@@ -74,71 +71,49 @@ $(function() {
   $.ajax({
     type: "get",
     url: BigNews.data_category,
-
     success: function(response) {
-      console.log(response);
-      let date = response.date.map(function(item) {
+      let date1 = response.date.map(function(item) {
+        //使用map映射出一个新的数组
         return { value: item.articles, name: item.name };
       });
-      console.log(date);
 
       //饼图
       let myChart = echarts.init(document.getElementById("pie_show")); //初始化,这里是不会变的
       option = {
-        backgroundColor: "#2c343c",
-
-        title: {
-          text: "分类文章数量比",
-          left: "center", //文章标题居中
-          top: 20,
-          textStyle: {
-            color: "#ccc"
-          }
-        },
-
         tooltip: {
           trigger: "item",
-          formatter: "{a} <br/>{b} : {c} ({d}%)"
+          formatter: "{a} <br/>{b}: {c} ({d}%)"
         },
-
-        visualMap: {
-          show: false,
-          min: 80,
-          max: 600,
-          inRange: {
-            colorLightness: [0, 1]
-          }
+        legend: {
+          orient: "vertical",
+          left: 10,
+          data: response.date
         },
         series: [
           {
             name: "分类名称",
             type: "pie",
-            radius: "55%",
-            center: ["50%", "50%"],
-            data: date,
-            roseType: "radius",
+            radius: ["50%", "70%"],
+            avoidLabelOverlap: false,
             label: {
-              color: "rgba(255, 255, 255, 0.3)"
+              normal: {
+                show: false,
+                position: "center"
+              },
+              emphasis: {
+                show: true,
+                textStyle: {
+                  fontSize: "16",
+                  fontWeight: "bold"
+                }
+              }
             },
             labelLine: {
-              lineStyle: {
-                color: "rgba(255, 255, 255, 0.3)"
-              },
-              smooth: 0.2,
-              length: 10,
-              length2: 20
+              normal: {
+                show: false
+              }
             },
-            itemStyle: {
-              color: "#c23531",
-              shadowBlur: 200,
-              shadowColor: "rgba(0, 0, 0, 0.5)"
-            },
-
-            animationType: "scale",
-            animationEasing: "elasticOut",
-            animationDelay: function(idx) {
-              return Math.random() * 200;
-            }
+            data: date1 //要按照它的格式才行
           }
         ]
       };
@@ -146,4 +121,93 @@ $(function() {
       myChart.setOption(option);
     }
   });
+  //分类访问量
+
+  // let myChart = echarts.init(document.getElementById("visit")); //初始化,这里是不会变的
+  // option = {
+  //   title: {
+  //     text: "阶梯瀑布图",
+  //     subtext: "From ExcelHome",
+  //     sublink: "http://e.weibo.com/1341556070/Aj1J2x5a5"
+  //   },
+  //   tooltip: {
+  //     trigger: "axis",
+  //     axisPointer: {
+  //       // 坐标轴指示器，坐标轴触发有效
+  //       type: "shadow" // 默认为直线，可选为：'line' | 'shadow'
+  //     },
+  //     formatter: function(params) {
+  //       var tar;
+  //       if (params[1].value !== "-") {
+  //         tar = params[1];
+  //       } else {
+  //         tar = params[0];
+  //       }
+  //       return tar.name + "<br/>" + tar.seriesName + " : " + tar.value;
+  //     }
+  //   },
+  //   legend: {
+  //     data: ["支出", "收入"]
+  //   },
+  //   grid: {
+  //     left: "3%",
+  //     right: "4%",
+  //     bottom: "3%",
+  //     containLabel: true
+  //   },
+  //   xAxis: {
+  //     type: "category",
+  //     splitLine: { show: false },
+  //     data: (function() {
+  //       var list = [];
+  //       for (var i = 1; i <= 11; i++) {
+  //         list.push("11月" + i + "日");
+  //       }
+  //       return list;
+  //     })()
+  //   },
+  //   yAxis: {
+  //     type: "value"
+  //   },
+  //   series: [
+  //     {
+  //       name: "辅助",
+  //       type: "bar",
+  //       stack: "总量",
+  //       itemStyle: {
+  //         barBorderColor: "rgba(0,0,0,0)",
+  //         color: "rgba(0,0,0,0)"
+  //       },
+  //       emphasis: {
+  //         itemStyle: {
+  //           barBorderColor: "rgba(0,0,0,0)",
+  //           color: "rgba(0,0,0,0)"
+  //         }
+  //       },
+  //       data: [0, 900, 1245, 1530, 1376, 1376, 1511, 1689, 1856, 1495, 1292]
+  //     },
+  //     {
+  //       name: "收入",
+  //       type: "bar",
+  //       stack: "总量",
+  //       label: {
+  //         show: true,
+  //         position: "top"
+  //       },
+  //       data: [900, 345, 393, "-", "-", 135, 178, 286, "-", "-", "-"]
+  //     },
+  //     {
+  //       name: "支出",
+  //       type: "bar",
+  //       stack: "总量",
+  //       label: {
+  //         show: true,
+  //         position: "bottom"
+  //       },
+  //       data: ["-", "-", "-", 108, 154, "-", "-", "-", 119, 361, 203]
+  //     }
+  //   ]
+  // };
+  // // 使用刚指定的配置项和数据显示图表。这里也是不会变得
+  // myChart.setOption(option);
 });
